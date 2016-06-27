affe13d9162a5efccf30775181d929b87561f5b5
Junfeng (Jeff) Li <autozimu@gmail.com>
2016-06-26 23:45:41 -0400

    Install swift development on osx.

--- .travis-install-swift.sh
+++ .travis-install-swift.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-set -ev
+set -euf -o pipefail
+
+VERSION=swift-DEVELOPMENT-SNAPSHOT-2016-06-20-a
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    export VERSION=swift-DEVELOPMENT-SNAPSHOT-2016-06-20-a
-
     if [[ ! -d "$HOME/.swift/${VERSION}-ubuntu14.04" ]]; then
         pushd ${PWD}
         mkdir -p $HOME/.swift
@@ -15,6 +15,15 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
         rm ${PKG}
         popd
     fi
-
     export PATH=$HOME/.swift/${VERSION}-ubuntu14.04/usr/bin:$PATH
+elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    if [[ ! -d "$HOME/.swift/${VERSION}" ]]; then
+        pushd ${PWD}
+        mkdir -p $HOME/.swift
+        cd $HOME/.swift
+        PKG=${VERSION}-osx.pkg
+        wget https://swift.org/builds/development/xcode/${VERSION}/${PKG}
+        sudo installer -pkg ${PKG} -target $HOME/.swift/${VERSION}
+    fi
+    export PATH=$HOME/.swift/${VERSION}/usr/bin:$PATH
 fi
