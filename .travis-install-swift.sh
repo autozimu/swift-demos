6eabb0c655584d9b9ca280da611d353d9a4e3c74
Junfeng (Jeff) Li <autozimu@gmail.com>
2016-06-26 22:29:39 -0400

    Fix cache.

--- .travis-install-swift.sh
+++ .travis-install-swift.sh
@@ -3,16 +3,18 @@
 set -ev
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    export SWIFT_VERSION=swift-DEVELOPMENT-SNAPSHOT-2016-06-20-a
+    export VERSION=swift-DEVELOPMENT-SNAPSHOT-2016-06-20-a
 
-    if [[ ! -d "$HOME/.swift" ]]; then
+    if [[ ! -d "$HOME/.swift/${VERSION}-ubuntu14.04" ]]; then
         pushd ${PWD}
         mkdir $HOME/.swift
         cd $HOME/.swift
-        wget https://swift.org/builds/development/ubuntu1404/${SWIFT_VERSION}/${SWIFT_VERSION}-ubuntu14.04.tar.gz
-        tar -zxf ${SWIFT_VERSION}-ubuntu14.04.tar.gz
+        PKG=${VERSION}-ubuntu14.04.tar.gz
+        wget https://swift.org/builds/development/ubuntu1404/${VERSION}/${PKG}
+        tar -zxf ${PKG}
+        rm ${PKG}
         popd
     fi
 
-    export PATH=$HOME/.swift/${SWIFT_VERSION}-ubuntu14.04/usr/bin:$PATH
+    export PATH=$HOME/.swift/${VERSION}-ubuntu14.04/usr/bin:$PATH
 fi
