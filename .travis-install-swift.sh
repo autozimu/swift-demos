33797fcd1cd7e86a1c9cbbc43ccdbc1c8e656e78
Junfeng (Jeff) Li <autozimu@gmail.com>
2016-06-26 22:03:11 -0400

    Enable cache & specify language explicitly.

--- .travis-install-swift.sh
+++ .travis-install-swift.sh
@@ -7,7 +7,7 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     mkdir -p $HOME/.swift
     export SWIFT_VERSION=swift-DEVELOPMENT-SNAPSHOT-2016-06-20-a
     wget https://swift.org/builds/development/ubuntu1404/${SWIFT_VERSION}/${SWIFT_VERSION}-ubuntu14.04.tar.gz
-    tar xzf ${SWIFT_VERSION}-ubuntu14.04.tar.gz
+    tar -zxvf ${SWIFT_VERSION}-ubuntu14.04.tar.gz
     export PATH=${PWD}/${SWIFT_VERSION}-ubuntu14.04/usr/bin:$PATH
     popd
 fi
