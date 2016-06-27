0ee1d8e24c0c1ba06f981df6d271462b5d0eed16
Junfeng (Jeff) Li <autozimu@gmail.com>
2016-06-26 22:19:38 -0400

    Fix install script.

--- .travis-install-swift.sh
+++ .travis-install-swift.sh
@@ -2,12 +2,17 @@
 
 set -ev
 
-if [[ "$TRAVIS_OS_NAME" == "linux" ]] && [[ ! -d "$HOME/.swift"  ]] ; then
-    pushd ${PWD}
-    mkdir -p $HOME/.swift
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     export SWIFT_VERSION=swift-DEVELOPMENT-SNAPSHOT-2016-06-20-a
-    wget https://swift.org/builds/development/ubuntu1404/${SWIFT_VERSION}/${SWIFT_VERSION}-ubuntu14.04.tar.gz
-    tar -zxf ${SWIFT_VERSION}-ubuntu14.04.tar.gz
-    export PATH=${PWD}/${SWIFT_VERSION}-ubuntu14.04/usr/bin:$PATH
-    popd
+
+    if [[ ! -d "$HOME/.swift" ]]; then
+        pushd ${PWD}
+        mkdir $HOME/.swift
+        cd $HOME/.swift
+        wget https://swift.org/builds/development/ubuntu1404/${SWIFT_VERSION}/${SWIFT_VERSION}-ubuntu14.04.tar.gz
+        tar -zxf ${SWIFT_VERSION}-ubuntu14.04.tar.gz
+        popd
+    fi
+
+    export PATH=$HOME/.swift/${SWIFT_VERSION}-ubuntu14.04/usr/bin:$PATH
 fi
